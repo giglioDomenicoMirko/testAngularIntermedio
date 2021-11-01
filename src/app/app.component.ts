1391e37394bda06c6d2cfa76c34d5f6b8e43ec54
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-11-01 08:52:00 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gdm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'testAngularIntermedio';
+}
