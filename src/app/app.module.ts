9c8b829c23fe94f9936b2fa149e5d73051ceedd3
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-11-01 12:40:16 +0100

    create routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderNavbarComponent } from './layout/header-navbar/header-navbar.component';
-import { HomeComponent } from './modules/home/home.component';
+
 import { CardViewComponent } from './modules/pages/card-view/card-view.component';
 import { ListViewComponent } from './modules/pages/list-view/list-view.component';
 import { NewUserFormComponent } from './modules/pages/new-user-form/new-user-form.component';
 import { OrdersListComponent } from './modules/pages/orders-list/orders-list.component';
+
+// MODULI
+import { HomeModule } from './modules/home.module';
+
+// COMPONENTI
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderNavbarComponent,
     HomeComponent,
-    CardViewComponent,
-    ListViewComponent,
-    NewUserFormComponent,
-    OrdersListComponent
+    // CardViewComponent,
+    // ListViewComponent,
+    // NewUserFormComponent,
+    // OrdersListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
