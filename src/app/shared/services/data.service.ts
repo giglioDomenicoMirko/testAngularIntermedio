e9921fc0f8516890316f88808357a49d70a88b19
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-11-02 22:23:31 +0100

    create method to get API data

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IUser } from 'src/app/models/User';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  userAnagrafica: IUser[] = [];
+
+  private userSubject = new BehaviorSubject<IUser[]>(this.userAnagrafica);
+  public user$ = this.userSubject.asObservable();
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAnagrafica(): Observable<IUser[]> {
+    return this.httpClient.get<IUser[]>('http://localhost:3000/anagrafica');
+  }
 }
